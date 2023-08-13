d16bea592a160e094d0925fe6ff16752a2f7e1cd
kalandjl <karj5903@gmail.com>
2023-08-12 21:34:26 -0700

    Async server fetching functions implemented

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,16 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-async function main() {
+async function main(run: () => {}) {
 
-    
+    return run()
 }
 
-main()
+
+export const runAsyncPrisma = async (run: () => {}) => {
+
+    let val = await main(run)
     .catch(e => {
         console.error(e.message)
     })
     .finally(async () => {
         await prisma.$disconnect()
+        return run()
     })
+
+    return val
+}
+
+
 
